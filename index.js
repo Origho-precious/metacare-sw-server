1ce32323e46394659e852f1e523bea4e9c086006
Origho-precious <Origho9@gmail.com>
2021-09-28 00:14:05 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const axios = require("axios");
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/films", async (req, res) => {
 	try {
